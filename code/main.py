2b30a13bba4ab17cc421e41a9da38f5b9adafe18
Alessandro Ramos <toprime13@gmail.com>
2020-11-30 19:11:43 -0300

    adicionado menu

--- code/main.py
+++ code/main.py
@@ -1,43 +1,44 @@
 # Importações
 from random import randint
+from time import sleep
 
 # Funções
+
+
+def menu():
+    while True:
+        print('-' * 42)
+        print('MENU'.center(42))
+        print('-' * 42)
+        print('0 - Sair')
+        print('1 - Iniciar')
+        print('-' * 42)
+        while True:
+            perguntaMenu = int(input('O que deseja fazer? '))
+            if perguntaMenu == 0:
+                print('-' * 42)
+                print('FINALIZANDO O PROGAMA...'.center(42))
+                print('-' * 42)
+                sleep(1.5)
+                break
+            elif perguntaMenu == 1:
+                print('-' * 42)
+                print('Iniciando...'.center(42))
+                print('-' * 42)
+                sleep(1.5)
+                iniciar()
+                break
+            print('Opção inválida. Tente novamente!')
+        if perguntaMenu == 0:
+            break
+
+def iniciar():
+    pass
 
 
 # Palavras
 
 listaPalavras = ['amarelo', 'amiga', 'amor', 'ave', 'avião', 'avó', 'balão', 'bebê']
-palavraFormando = ''
 
-# Sorteio 
-
-númeroSorteado = randint(0 , 7)
-palavraSelecionada = listaPalavras[númeroSorteado]
-
-# Jogo da Forca
-
-print(f'A palavra selecionada foi {palavraSelecionada} e tem {len(palavraSelecionada)} letras.')
-for letra in range (len(palavraSelecionada)):
-    print('- ', end='')
-print()
-
-letraRepetida = ''
-while True:
-    letraChutada = str(input('Qual a letra? ')).lower()
-    perguntaLetraRepetida = 's'
-    if letraChutada in letraRepetida:
-        while True:
-            perguntaLetraRepetida = str(input(f'A letra {letraChutada} é repetida. \nDeseja mesmo escolher essa letra? [S/N] ')).lower()
-            if perguntaLetraRepetida in 'SsNn':
-                break
-    if perguntaLetraRepetida == 's':
-        if letraChutada in palavraSelecionada:
-            print('Letra certa.')
-            palavraFormando += letraChutada
-            if palavraFormando == palavraSelecionada:
-                break
-        else:
-            print('Letra errada.')
-    else:
-        print('Letra desconsiderada.')  
-    letraRepetida += letraChutada
+# Iniciar
+menu()
